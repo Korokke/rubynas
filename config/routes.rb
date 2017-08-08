5a0c55f2e41febbcd11ecc294bb553a07e2341a0
Korokke <rhfhzp001@gmail.com>
2017-08-08 18:08:07 +0900

    Fix signup error

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-
   # User Control
-  post "/users", to: "users#create"
-  patch "/users/:name", to: "users#update"
-  delete "/users/:name", to: "users#destroy"
+  post "/users", to: "users#create", as: "users"
+  patch "/users/:name", to: "users#update", as: "update_user"
+  delete "/users/:name", to: "users#destroy", as: "destroy_user"
 
   # Session Control
-  post "/sessions", to: "sessions#create"
-  delete "/sessions/:id", to: "sessions#destroy", as: 'session'
+  post "/sessions", to: "sessions#create", as: "sessions"
+  delete "/sessions/:id", to: "sessions#destroy", as: "session"
 
   # User Page
-  get "/:name", to: "users#show", as: 'user'
+  get "/:name", to: "users#show", as: "user"
 
   root "users#index"
 end
